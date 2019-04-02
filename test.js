5b468cd7689921bf7800c58f286f0cf7194381f8
Matthew Youngbar <matthewyoungbar@users.noreply.github.com>
2019-04-02 13:07:32 -0400

    json test

--- test.js
+++ test.js
@@ -6,22 +6,32 @@ var d = new Date();
 print(d);
 var x = 0;
 var numlikes = 0;
+var num = 0;
 function addlike() {
-  const fs = require('fs');
-  var num = 0;
-  fs.readFile('likes.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
-
-    // Converting Raw Buffer to text
-    // data using tostring function.
-    num = int(data);
-  })
+  // const fs = require('fs');
+  $().ready(function(){
+      $.getJSON( "data.json", function( data ) {
+        num = data;
+        console.log(data);
+        $("#text").html(data["text"]);
+    });
+  });
+  // fs.readFile('likes.txt', 'utf-8', (err, data) => {
+  //   if (err) throw err;
+  //
+  //   // Converting Raw Buffer to text
+  //   // data using tostring function.
+  //   num = int(data);
+  // })
   num += 1;
-  document.write(num);
+  $().ready(function() {
+     $("#text").html(num);
+  });
+  // document.write(num);
   console.log(num);
-  fs.writeFile('likes.txt', string(num), (err) => {
-    if (err) throw err;
-  })
+  // fs.writeFile('likes.txt', string(num), (err) => {
+  //   if (err) throw err;
+  // })
   // (document.getElementById("pagelikes"))
   numlikes = num;
 }
