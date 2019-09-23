a4afcb6325573bb51302ced8e19f6f4d6bd1c517
Matthew Youngbar <matthewyoungbar@users.noreply.github.com>
2019-09-23 14:01:29 -0400

    Big Style Changes

--- json.js
+++ json.js
@@ -1,6 +1,6 @@
-$().ready(function(){
-    $.getJSON( "/data.json", function( data ) {
-    console.log(data);
-    $("#text").html(data["text"]);
-  });
-});
+// $().ready(function(){
+//     $.getJSON( "/data.json", function( data ) {
+//     console.log(data);
+//     $("#text").html(data["text"]);
+//   });
+// });
